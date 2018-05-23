65c1c7531635ee35ff4e84f2b91218410e4db2cb
Morgan Williams <morgan.williams@csiro.au>
2018-05-23 16:28:53 +0800

    Update Intro, Datasources, Ignore

--- datasource.py
+++ datasource.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from boto.s3.connection import S3Connection
 from pathlib import Path
     
@@ -11,3 +12,13 @@ def download_data(filename_remote, filename_local):
     bucket = conn.get_bucket('c3dis-geochem-data', validate=True)
     data = bucket.get_key(filename_remote)
     data.get_contents_to_filename(filename_local)
+    
+    
+def load_df(filename_local):
+    if Path(filename_local).exists():
+        return pd.read_pickle(filename_local)
+    else:
+        try:
+            download_data(filename_local, filename_local)
+        except:
+            'Error: Data needs to be downloaded first.'
